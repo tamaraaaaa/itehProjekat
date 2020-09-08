f7f2291fd1173dbf6bafd0931e3fea7e152beb89
tamaraaaaa <58712881+tamaraaaaa@users.noreply.github.com>
2020-09-08 03:06:22 +0200

    Update header.php
    Dodat logo u nav. meniju

--- korisnik/template/header.php
+++ korisnik/template/header.php
@@ -24,7 +24,7 @@
                         <span class="icon-bar"></span>
                       </button>
                       <a class="navbar-brand" href="index.php">
-                          <img id="logo" src="" alt="biblioiteha_logo"/>
+                          <img id="logo" src="logooooo.jpg" alt="biblioiteha_logo"/>
                       </a>
                   </div>
                   <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
